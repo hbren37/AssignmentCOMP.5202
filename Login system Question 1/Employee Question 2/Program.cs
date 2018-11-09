49654f6a865931d894dec7b71fec1682bb119d9a
Helen Brennan <helen7bren@gmail.com>
2018-11-09 13:39:36 +1300

    Assignment

---
+++ Login system Question 1/Employee Question 2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Question_2
+{//Helen Brennan 9981703
+    class Employee
+    {
+        public string Fullname { get; set; }
+        public double Tax { get; set; }
+        public double Gross { get; set; } // getters and setters for employee info all public
+
+
+
+        public Employee(string fullname, double tax, double gross)
+        {
+            Fullname = fullname; //constructor
+            Tax = tax;
+            Gross = gross;
+            Console.Write("Employee record created");
+
+        }
+        public double CalcNet()
+        {
+            return Gross * (1 - Tax / 100);//method to calculate net salary
+        }
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("Welcome to Payroll 345, please enter your employees details\n");
+            Console.Write("Please enter employee full name\n");
+            string fullname = Console.ReadLine();
+            Console.Write("Please enter required tax rate\n");
+            double tax = double.Parse(Console.ReadLine());
+            Console.Write("Please enter annual gross salary\n");
+            double gross = double.Parse(Console.ReadLine());
+
+
+            Employee e1 = new Employee(fullname, tax, gross);
+
+            Console.WriteLine($"Full name: {e1.Fullname}\nTax: {e1.Tax}%\nAnnual Gross Salary: {e1.Gross}");
+            Console.Write($"Net Salary: {e1.CalcNet()}");
+
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Login system Question 1/Login system Question 1/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login_system_Question_1
+{//Helen Brennan 9981703
+
+        class Staffuser //new class for staff members 
+        {
+            public string username;
+            public string password;
+            //class includes, the members username and password
+
+            public string Username //public get and set for username making it accesible to anyone
+            {
+                get { return username; }
+                set { username = value; }
+            }
+
+            public string Password
+
+            {
+                private get { return password; } //private get for password so only certain people can access
+                set { password = value; }
+
+            }
+
+            public Staffuser(string username, string password)
+            {
+                Username = username;
+                Password = password;
+            }
+            public string Userset()
+            {
+                return "Your Username and password has been accepted and set";//method to set employees username and password
+
+            }
+
+
+            class Program
+            {
+                static void Main(string[] args)
+                {//Question One
+                    Console.Write("-------Rotorua Thermal Holiday Park----------\n");
+                    Console.Write("Welcome! All staff must create a Username and Password to login and continue\n");
+
+                    Console.Write("Login System\n");
+
+                    Console.WriteLine("Please enter a username containing at least 8 characters");
+
+                    string username = Console.ReadLine();
+                    int lenght = username.Length;// counting lenght of username string
+
+                if (lenght <= 8)
+                {
+
+                    Console.WriteLine("Username accepted\n");
+                }
+                else
+                {
+                    Console.WriteLine("Username does not meet specified conditions please try again");
+                }
+
+
+
+
+
+
+
+
+                Console.Write("Please enter a password that contains 8 characters for your account\n");
+                    string password = Console.ReadLine();
+                    Console.Write("Please re-enter your password\n");
+                    string password2 = Console.ReadLine();
+
+                    if (password == password2)
+                    {
+                        Console.Write("Password has been accepted");
+                    }
+                    else
+                    {
+                        Console.Write("Passwords do not match please try again");
+
+
+
+                    }
+
+                    Staffuser staffuser1 = new Staffuser(username, password);
+
+                    Console.WriteLine(staffuser1.Userset());// making new staff object 
+
+
+
+
+                    Console.ReadLine();
+                }
+                
+        
+        }
+    }
+}
